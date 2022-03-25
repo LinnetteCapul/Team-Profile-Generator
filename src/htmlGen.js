c77318ba2efe54c45f19ee9a97660f50a2a40171
LinnetteCapul <linnette.capul@gmail.com>
2022-03-25 15:24:34 -0700

    added mainMenu function and set up for engineer and intern prompt questions in index.js

--- src/htmlGen.js
+++ src/htmlGen.js
@@ -1,15 +1,15 @@
 const generateManagerCard = (manager) => {
     return `
-    <div class="card">
-        <h3>${manager.getName}<h3>
-        <p><i class="fa-solid fa-mug=hot"></i>${manager.getRole()}</p>
-        <div class="card-body>
-            <ul class="list-group list-group-flush">
-            <li class="list-group-item">ID: ${manager.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-            <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
-            </ul>
+        <div class="card">
+            <h3>${manager.getName}<h3>
+            <p><i class="fa-solid fa-mug=hot"></i>${manager.getRole()}</p>
+            <div class="card-body>
+                <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${manager.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
+                </ul>
+            </div>
         </div>
-    </div>
     `
 }
